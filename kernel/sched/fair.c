7eefa57486e7362c8aaa15bf3e96afcc3bd5d2bf
Diep Quynh <remilia.1505@gmail.com>
2021-05-10 20:42:48 +0200

    Revert "sched/fair: Don't let tasks slip away from gold to silver cluster".
    If we ever wanted to stop big tasks from being balanced out to smaller cluster,
    it had already been done by checking the tasks in can_migrate_task() with
    schedtune
    
    This commit, however, introduces a bug that light tasks including non-big tasks
    not being balanced out to smaller cluster, resulting in big cluster being
    occasionally overloaded
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10970,8 +10970,7 @@ static struct rq *find_busiest_queue(struct lb_env *env,
 		 */
 		if (env->sd->flags & SD_ASYM_CPUCAPACITY &&
 		    capacity_of(env->dst_cpu) < capacity &&
-		    (rq->nr_running == 1 || (rq->nr_running == 2 &&
-		     task_util(rq->curr) < sched_small_task_threshold)))
+ 		    rq->nr_running == 1)
 			continue;
 
 		wl = weighted_cpuload(rq);
