5dd5d74c60b901a7dd5dc39b2b8904366ab5e82b
TheVoyager0777 <l727127746@outlook.com>
2021-10-09 04:31:03 -0700

    fix complie

--- build.sh
+++ build.sh
@@ -1,10 +1,40 @@
 #!/bin/bash
+ANYKERNEL3_DIR=$PWD/AnyKernel3
+FINAL_KERNEL_ZIP=kernel-tucana-r-VoyagerIII-$(git rev-parse --short=7 HEAD).zip
+IMAGE_GZ=$PWD/out/arch/arm64/boot/Image.gz
+
 args="-j$(nproc --all) \
 O=out \
 ARCH=arm64 \
 CLANG_TRIPLE=/home/user/cer/tc/bin/aarch64-linux-gnu- \
 CROSS_COMPILE=/home/user/cer/tc/bin/aarch64-linux-gnu- \
-CC=/home/user/cer/clang-r399163b/bin/clang \
-CROSS_COMPILE_ARM32=/home/user/cer/tc/bin/arm-linux-gnueabi- "
+CC=/home/user/cer/clang-r433403/bin/clang \
+CROSS_COMPILE_ARM32=/home/user/cer/tc/bin/arm-linux-gnueabi- 
+AR="llvm-ar" \
+NM="llvm-nm" \
+OBJCOPY="llvm-objcopy" \
+OBJDUMP="llvm-objdump" \
+STRIP="llvm-strip" "
 make ${args} tucanas_defconfig
 make ${args}
+
+echo "**** Verify target files ****"
+if [ ! -f "$IMAGE_GZ" ]; then
+    echo "!!! Image.gz not found"
+    exit 1
+fi
+
+echo "**** Moving target files ****"
+mv $IMAGE_GZ $ANYKERNEL3_DIR/Image.gz
+
+echo "**** Time to zip up! ****"
+cd $ANYKERNEL3_DIR/
+zip -r9 $FINAL_KERNEL_ZIP * -x .git README.md *placeholder
+
+echo "**** Removing leftovers ****"
+cd ..
+rm $ANYKERNEL3_DIR/Image.gz
+
+mv -f $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP out/
+
+echo "Check out/$FINAL_KERNEL_ZIP"
